a841e73185cdf94888d2d7867fc7524029a9202e
Ketan Mahajan <ketan@platform9.com>
2019-05-24 08:44:57 -0400

    Added function `analyze`. - Takes a string as input - Return a sentiment value (between -1 and 1)

---
+++ analyzer.py
@@ -0,0 +1,14 @@
+import sys
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+nltk.download('vader_lexicon')
+sia = SentimentIntensityAnalyzer()
+
+def analyze(str):
+    si = sia.polarity_scores(str)
+    print si
+    return si['compound']
+
+print 'Text: ' + sys.argv[1]
+print 'Sentiment: ' + str(analyze(sys.argv[1]))
